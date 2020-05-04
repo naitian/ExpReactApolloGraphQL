ec90c1c347764dd133bff199b75e97f52bd927d0
Naitian Zhou <naitian@naitian.org>
2020-05-04 00:37:16 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import RoutingApp from "./modules/RoutingApp.js";
+
+ReactDOM.render(<RoutingApp />, document.getElementById("root"));
---
+++ src/modules/RoutingApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class RoutingApp extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello, world!</p>
+      </div>
+    );
+  }
+}
+
+export default RoutingApp;
